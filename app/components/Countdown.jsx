5617a2126a3f98a0f69dfdd75850fbaf67fef432
Joe Bimmerle <jbimmerle@yahoo.com>
2017-03-11 12:22:19 -0500

    Added clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,10 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = (props) => {
   return (
     <div>
-      <h1 className="text-center page-title">Countdown</h1>
-      <p className="text-center">This is the Countdown component.</p>
+      <Clock totalSeconds={129}/>
     </div>
   );
 }
